1c804b1e974e1119043ae9942d4e9b4f83620205
newwynn <nevin.r@helixo.co>
2025-07-16 02:41:54 +0530

    feat: added stories table

--- app/routes/app.listings.tsx
+++ app/routes/app.listings.tsx
@@ -4,25 +4,96 @@ import {
   Page,
   Text,
   BlockStack,
+  IndexTable,
+  LegacyCard,
+  useIndexResourceState,
+  Badge,  
 } from "@shopify/polaris";
 import { useCallback } from "react";
 
 export default function Index() {
 
-  const handleOpen: () => void = useCallback(async () => {
-    const productSelector = await shopify?.resourcePicker({ type: "product" });
+     const orders = [
+    {
+      id: '1020',
+      order: '#1020',
+      date: 'Jul 20 at 4:34pm',
+      customer: 'Jaydon Stanton',
+      total: '$969.44',
+      paymentStatus: <Badge progress="complete">Paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },
+    {
+      id: '1019',
+      order: '#1019',
+      date: 'Jul 20 at 3:46pm',
+      customer: 'Ruben Westerfelt',
+      total: '$701.19',
+      paymentStatus: <Badge progress="partiallyComplete">Partially paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },
+    {
+      id: '1018',
+      order: '#1018',
+      date: 'Jul 20 at 3.44pm',
+      customer: 'Leo Carder',
+      total: '$798.24',
+      paymentStatus: <Badge progress="complete">Paid</Badge>,
+      fulfillmentStatus: <Badge progress="incomplete">Unfulfilled</Badge>,
+    },
+  ];
+  const resourceName = {
+    singular: 'order',
+    plural: 'orders',
+  };
 
-    if (productSelector) {
-      console.log("Selected Products", productSelector);
-    }
-  }, []);
+  const {selectedResources, allResourcesSelected, handleSelectionChange} =
+    useIndexResourceState(orders);
+
+  const rowMarkup = orders.map(
+    (
+      {id, order, date, customer, total, paymentStatus, fulfillmentStatus},
+      index,
+    ) => (
+      <IndexTable.Row
+        id={id}
+        key={id}
+        selected={selectedResources.includes(id)}
+        position={index}
+      >
+        <IndexTable.Cell>
+          <Text variant="bodyMd" fontWeight="bold" as="span">
+            {order}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{date}</IndexTable.Cell>
+        <IndexTable.Cell>{customer}</IndexTable.Cell>
+        <IndexTable.Cell>
+          <Text as="span" alignment="end" numeric>
+            {total}
+          </Text>
+        </IndexTable.Cell>
+        <IndexTable.Cell>{paymentStatus}</IndexTable.Cell>
+        <IndexTable.Cell>{fulfillmentStatus}</IndexTable.Cell>
+      </IndexTable.Row>
+    ),
+  );
+    const handleOpen: () => void = useCallback(async () => {
+      const productSelector = await shopify?.resourcePicker({
+        type: "product",
+      });
+
+      if (productSelector) {
+        console.log("Selected Products", productSelector);
+      }
+    }, []);
 
   return (
     <Page
       title="Your Product Stories"
       subtitle="View, edit, and manage the origin stories shown after checkout for each product."
       primaryAction={{
-        content: "Add Story",
+        content: "Tell a Product’s Story",
         onAction: handleOpen,
       }}
     >
@@ -37,6 +108,28 @@ export default function Index() {
               </Text>
             </BlockStack>
           </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <LegacyCard>
+            <IndexTable
+              resourceName={resourceName}
+              itemCount={orders.length}
+              selectedItemsCount={
+                allResourcesSelected ? 'All' : selectedResources.length
+              }
+              onSelectionChange={handleSelectionChange}
+              headings={[
+                {title: 'Order'},
+                {title: 'Date'},
+                {title: 'Customer'},
+                {title: 'Total', alignment: 'end'},
+                {title: 'Payment status'},
+                {title: 'Fulfillment status'},
+              ]}
+            >
+              {rowMarkup}
+            </IndexTable>
+          </LegacyCard>
         </Layout.Section>
       </Layout>
     </Page>
